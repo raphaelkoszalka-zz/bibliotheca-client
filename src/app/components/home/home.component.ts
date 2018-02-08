ff18a4dec544443dccfe8492017c267e36d59b95
Raphael Luiz Martins Koszalka <raphael@cochise.com.br>
2018-02-07 23:52:51 -0200

    backgroundStyle config object

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -54,7 +54,7 @@ export class HomeComponent implements OnInit {
       'background-size': 'cover',
       '-webkit-background-size': 'cover',
       'background-position': 'center center',
-      ' background': 'lightblue'
+      'background-color': '#4e5d7c'
     };
 
     // better fluid, better loader than image loading in blocks
